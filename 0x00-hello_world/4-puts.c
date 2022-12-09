99b0f4b2055014e25b176d3f2f622d48062a95c7
Anas <anas0374@gmail.com>
2022-12-08 18:28:57 -0800

    1212121c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
-/*
- * print using puts funtion
+/**
+ * main - print using puts funtion
+ *
+ * Return: 0
 */
-int main (void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
-
-
-
-
-
+	return (0);
 }
