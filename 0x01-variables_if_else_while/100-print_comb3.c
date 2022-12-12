a55911718305e7e83715107f6baace2b0faa7aea
Anas <anas0374@gmail.com>
2022-12-11 18:38:05 -0800

    fsf

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+	int nu;
+
+	for (num = 0; num <= 9; num++)
+	{
+		for  (nu = 1; nu <= 9; nu++)
+		{
+			putchar((num % 10) + '0');
+			putchar((nu % 10) + '0');
+			if (num == 8 && nu == 9)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
