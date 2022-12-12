eca6eeaf403c176c59f08950f67eaeeaca91fba4
Anas <anas0374@gmail.com>
2022-12-11 17:38:21 -0800

    6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= '9'; x++)
+		printf("%d\n", x);
+	return (0);
+}
