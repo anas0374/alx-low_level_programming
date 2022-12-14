7454c9381fe6329779d4c07757df06f947b8f2cd
Anas <anas0374@gmail.com>
2022-12-13 20:36:29 -0800

    dfgsdgsdfg

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  jack_bauer - prints every minute of the day of Jack Baue.
+ *  Return: 1 if c is lowercase Returns ,0 otherwise.
+ */
+void jack_bauer(void)
+{
+	int x;
+
+	x = x * 24 * 60;
+	_putchar('0' + x);
+}
+
