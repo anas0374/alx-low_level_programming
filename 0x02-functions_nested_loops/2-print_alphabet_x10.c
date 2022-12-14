984fdafaa29c5383d19b558ca5d5b278a85ddec9
Anas <anas0374@gmail.com>
2022-12-13 18:24:27 -0800

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print letter 10 time
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int x = 0;
+
+	while (x < 10)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n);
+		x++;
+	}
+}
+
