eb6cd1d990514f9967a50b5be917b97b44e32273
Anas <anas0374@gmail.com>
2022-12-14 17:57:36 -0800

    wewewe

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,17 +6,17 @@
  */
 void jack_bauer(void)
 {
-	int i,m;
+	int i, m;
 
 	for (i = 0 ; i <= 23; i++)
 	{
 		for (m = 0 ; m < 60 ; m++)
 		{
-			_putchar(i/10 + '0');
-			_putchar(i%10 + '0');
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
 			_putchar(':');
-			_putchar(m/10 + '0');
-			_putchar(m%10 + '0');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
 			_putchar('\n');
 		}
 	}
