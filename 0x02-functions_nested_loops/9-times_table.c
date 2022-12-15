bd97c8c4f2144fc088b0d8f9adeb7c6161a1d52a
Anas <anas0374@gmail.com>
2022-12-14 18:29:52 -0800

    gjkhjkh

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *   times_table - prints every minute of the day of Jack Bae.
+ *  Return: 1 if c is lowercase Returns ,0 otherwise.
+ */
+void times_table(void)
+{
+	int i, m, x;
+
+	for (i = 0 ; i <= 9; i++)
+	{
+		for (m = 0 ; m < 10 ; m++)
+		{
+			x = i * m;
+			if (x > 9)
+			{
+				_putchar(x / 10 + '0');
+				_putchar(x % 10 + '0');
+			}
+			else
+			{
+				_putchar(x + '0');
+			}
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+	}
+}
