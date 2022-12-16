e813de56df8ff953dc5947124ac1b7c130c99e40
Anas <anas0374@gmail.com>
2022-12-16 10:05:25 -0800

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: FizzBuzz
+ * Fizz
+ * Return: void
+ */
+
+void main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 5 == 0) && (i % 3 == 0))
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+		else
+		{
+			printf("%d", i);
+			printf(" ");
+		}
+	}
+}
