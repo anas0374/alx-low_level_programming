6d49525e1e57cf610d5897657ccf2645e31b187a
Anas <anas0374@gmail.com>
2022-12-19 15:11:47 -0800

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  print_rev -  prints a string, in reverse, followed by a new line
+ *  @s: int to pointer
+ *  Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int a;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (a = i; a == 0 ; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
