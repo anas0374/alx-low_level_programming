2159395dcffe7f66942e53607a0fd941ad02764a
Anas <anas0374@gmail.com>
2022-12-19 14:16:55 -0800

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  void swap_int -  swaps the values of two integers
+ *  @a: int to pointer
+ *  @b: int to pointer
+ *  Return: 0
+ */
+
+void void swap_int(int *a, int *b)
+{
+	int x;
+
+	x = *a;
+	*a = *b;
+	*b = x;
+
+}
+~
+
