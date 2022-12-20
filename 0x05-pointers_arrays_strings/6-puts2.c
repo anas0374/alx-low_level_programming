c1485533e8b9be61843b69e5e846d625d4ddec63
Anas <anas0374@gmail.com>
2022-12-19 16:50:30 -0800

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - with the first character, followed by a new line.
+ * @str: string
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	int a;
+
+	while (str[i] != '\0')
+	{
+		i++
+	}
+	for (x = 0 ; x > i ; x = x + 2)
+	{
+		_putchar(str[x]);
+	}
+
+}
