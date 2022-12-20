3a1bf37e445fcda7606c0e6e35576a16d2146ed9
Anas <anas0374@gmail.com>
2022-12-19 17:10:08 -0800

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,14 +7,14 @@
 void puts_half(char *str)
 {
 	int i = 0;
-	int n;
+	int n, a;
 
 	while (str[i] != '\0')
 	{
 		i++;
 	}
-	n = ((i - 1) / 2);
-	for (n; n <= n * 2; n++)
+	a = ((i - 1) / 2);
+	for (n = a; n <= n * 2; n++)
 	{
 		_putchar(str[n]);
 	}
