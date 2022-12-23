bbea8b5c58cc4556820b895844f2b49bb1f7188a
Anas <anas0374@gmail.com>
2022-12-22 16:06:47 -0800

    m

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,14 +11,15 @@
 
 char *_strncat(char *dest, char *src)
 {
-	int i, j;
+	int i;
+	int j;
 
 	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
-	
-	j = 0;}
+	}
+	j = 0;
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
