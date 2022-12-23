4fd63c166831412e459047231c9ac6d4607b1fa0
Anas <anas0374@gmail.com>
2022-12-22 16:26:08 -0800

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ * @n: intger
+ *
+ * Return: void
+ */
+char *_strncat(char *dest, char *src, int n);
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++
+	}
+	j = n - 1;
+	while (src[j] != '\0')
+	{
+		dest[i] = scr[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
